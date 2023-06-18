742dd2f4043e2494f567ffbd0266c1363373bdce
youssef-abjaou <judasabjaou13@gmail.com>
2023-06-18 19:22:37 +0000

    program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *str1 = "abcdefghijklmnopqrstuvwxyz";
+	const char *str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	int i = 0;
+
+	for (i; str1[i] != '\0'; i++)
+	{
+		putchar(str1[i]);
+	}
+	for (int i = 0; str2[i] != '\0'; i++)
+	{
+		putchar(str2[i]);
+	}
+
+	return (0);
+}
