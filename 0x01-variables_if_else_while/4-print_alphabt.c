2e353ccf7035736dffae64dfa1b082d8f17b7150
youssef-abjaou <judasabjaou13@gmail.com>
2023-06-18 20:11:00 +0000

    program that prints the alphabet in lowercase, followed by a new line.except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+
+	return (0);
+}
