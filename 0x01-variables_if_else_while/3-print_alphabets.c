2717202bf8d5cf47baf0f739dd66cdcb4295b72d
youssef-abjaou <judasabjaou13@gmail.com>
2023-06-18 19:44:54 +0000

    program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	const char *str1 = "abcdefghijklmnopqrstuvwxyz";
 	const char *str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	int i;
 
-	for (int i = 0; str1[i] != '\0'; i++)
+	for (i = 0; str1[i] != '\0'; i++)
 	{
 		putchar(str1[i]);
 	}
-	for (int i = 0; str2[i] != '\0'; i++)
+	i = 0;
+	for (i; str2[i] != '\0'; i++)
 	{
 		putchar(str2[i]);
 	}
