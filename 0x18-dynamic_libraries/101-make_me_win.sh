9d616519fd2abd72aac68844ef880f7fc9107421
youssef-abjaou <130617788+youssef-abjaou@users.noreply.github.com>
2023-09-18 19:42:27 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/youssef-abjaou/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/youssef-abjaou/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
